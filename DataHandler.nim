628c73fd8b0f6caee5fbe609b2a2956a376e4215
Aaron Fang <7587244+aaronjfang@users.noreply.github.com>
2020-12-05 22:50:07 -0800

    implement loading files from disk into memory

--- DataHandler.nim
+++ DataHandler.nim
@@ -1,6 +1,7 @@
 import httpclient
 import json
 import parsecsv
+import streams
 import os
 import strutils
 
@@ -21,16 +22,40 @@ proc newDataHandler*(apiKeyFile:File): DataHandler =
     result = DataHandler(apiKey: apiKeyFile.readLine)
 
 #pulls data from AlphaVantage and saves it to [project root]/datadir/[ticker name]/
-proc getHistoricDataFor*(a: DataHandler, ticker: string, interval: string, slice: string) = 
+proc getHistoricDataFor*(a: DataHandler, ticker: string, interval: string, slice: string): string {.discardable}= 
     var
         query = url&"function=TIME_SERIES_INTRADAY_EXTENDED"&"&symbol="&ticker&"&interval="&interval&"&slice="&slice&"&adjusted=true&apikey="&a.apiKey
         content = client.getContent(query)
-    
-    if os.dirExists(os.getCurrentDir()&"/datadir/"&toLowerAscii(ticker)):
-        writeFile(os.getCurrentDir()&"/datadir/"&ticker&"/"&interval&slice&".csv", content)
+    let 
+        tickerdir:string = os.getCurrentDir()&"/datadir/"&toLowerAscii(ticker)
+        filedir:string = tickerdir&"/"&interval&slice&".csv"
+    if os.dirExists(tickerdir):
+        writeFile(filedir, content)
     else:
-        os.createDir(os.getCurrentDir()&"/datadir/"&toLowerAscii(ticker))
-        writeFile(os.getCurrentDir()&"/datadir/"&ticker&"/"&interval&slice&".csv", content)
+        os.createDir(tickerdir)
+        writeFile(filedir, content)
+    result = filedir
 
+#take dir and load all data to memory (note: I don't really like returning a CsvParser due to its bulkiness... I will need to make a new type to store the time-series data)
+proc loadHistoricDataFromDisk*(a:DataHandler, tickerdirectory:string): CsvParser =
+    var parser: CsvParser
+    for kind, path in walkDir(tickerdirectory):
+        echo path
+        var stream = newFileStream(path, fmRead)
+        open(parser, stream)
+    result = parser 
+
+    
 #test this puppy out with TSLA 
-newDataHandler(open("api_key.txt")).getHistoricDataFor("tsla", "15min", "year1month1")
+var handler:DataHandler = newDataHandler(open("api_key.txt"))
+handler.getHistoricDataFor("tsla", "15min", "year1month1")
+
+#try to load a CSV
+var csv = handler.loadHistoricDataFromDisk(os.getCurrentDir()&"/datadir/tsla/")
+
+#output CSV contents
+csv.readHeaderRow()
+while csv.readRow():
+  echo "new row: "
+  for col in items(csv.headers):
+    echo "##", col, ":", csv.rowEntry(col), "##"
